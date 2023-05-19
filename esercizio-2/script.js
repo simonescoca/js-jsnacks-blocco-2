f342af88105ecd1a2a44f86e306f5b51bb95d65d
simonescoca <simone.scoca@icloud.com>
2023-05-19 18:01:21 +0200

    added some comments

--- esercizio-2/script.js
+++ esercizio-2/script.js
@@ -2,15 +2,19 @@
 // Ogni volta che sbaglia stampiamo in console un messaggio che gli/le dice se il numero da indovinare è maggiore o minore.
 // Una volta che ha indovinato, stampiamo in console il numero di tentativi totali.
 
+
+// Genero un numero casuale
 const randomNumber = parseInt ( Math.random () * 100) + 1
+
+// Dichiaro la variabile userNumber
 let userNumber = 0
 
-console.log (randomNumber)
 
+// ciclo while che s'interrompe quando l'utente indovina il numero
 while (userNumber != randomNumber ) {
     userNumber = prompt ("Indovina il numero che ho generato compreso tra 1 e 100, estremi inclusi")
 
-    if ( ( isNaN (userNumber) || parseFloat (userNumber) !== parseInt (userNumber) || parseFloat (userNumber) < 1 || parseFloat (userNumber) > 100) ) {
+    if (isNaN (userNumber) || parseFloat (userNumber) !== parseInt (userNumber) || parseFloat (userNumber) < 1 || parseFloat (userNumber) > 100) {
         console.log ("C'è un errore di scrittura dei dati")
 
     } else if (userNumber > randomNumber) {
@@ -18,6 +22,7 @@ while (userNumber != randomNumber ) {
 
     } else if (userNumber < randomNumber) {
         console.log ("Il numero da indovinare è più grande")
+        
     } else {
         console.log ("Hai vinto!")
     }
