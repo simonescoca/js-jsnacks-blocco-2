b487c5e225b67b3da27317f933dc72abf9943ca8
simonescoca <simone.scoca@icloud.com>
2023-05-23 12:25:58 +0200

    func() creates liElements

---
+++ jsnack-plus/script.js
@@ -0,0 +1,20 @@
+// Generare una lista usando una funzione:
+//   - nell'html ci dovrà essere esclusivamente un elemento ul parent
+//   - attraverso una funzione creiamo un singolo elemento html di tipo li
+//   - poi ne aggiungiamo una decina all'ul, sempre via js
+
+// BONUS: Se clicchiamo sul li il suo testo verrà sbarrato
+
+const ulElement = document.getElementById ("ul")
+const userWord = prompt ("Che inseriamo 10 volte nella lista?")
+
+function createElementLi (liContent) {
+    const liElement = document.createElement ("li")
+    liElement.append (liContent)
+    return liElement
+}
+
+for (let i = 0 ; i < 10 ; i++) {
+    const liElement = createElementLi (userWord)
+    ulElement.appendChild (liElement)
+}
